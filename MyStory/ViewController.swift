55ecff61122a1a07aff21698db8f92e77104916d
Jing <60806325+markov42@users.noreply.github.com>
2023-02-28 02:57:15 -0500

    added MyStory

---
+++ MyStory/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  MyStory
+//
+//  Created by Jing Liu on 2/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+//    @IBAction func didTapLink(_ sender: UITapGestureRecognizer) {
+//        if let tappedView = sender.view {
+//            performSegue(withIdentifier: "detailSegue", sender: tappedView)
+//        }
+//    }
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//
+//        if segue.identifier == "detailSegue",
+//            let tappedView = sender as? UIView,
+//            let detailViewController = segue.destination as? DetailViewController {
+//
+//
+//        }
+//    }
+    
+}
+
+
